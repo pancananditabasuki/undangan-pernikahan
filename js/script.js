3e8be27db6d7900943c07ebb602896132323d2eb
akbar tawakal pancanandita <pancadonald@gmail.com>
2024-07-29 20:11:23 +0700

    undangan

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Start Gallery
+
+// End Gallery
+
+// Start Story
+
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2700,
+  delay: 450,
+  reset: true,
+});
+
+// Start Gallery
+sr.reveal(".gallery span", { delay: 200, origin: "bottom" });
+sr.reveal(".gallery h2", { delay: 200, origin: "bottom" });
+sr.reveal(".gallery p", { delay: 200, origin: "bottom" });
+sr.reveal(".gallery", { delay: 200, origin: "bottom" });
+// End Gallery
+
+// Start Story
+sr.reveal(".timeline-heading", { delay: 200, origin: "bottom" });
+sr.reveal(".timeline-body", { delay: 200, origin: "bottom" });
+sr.reveal(".timeline-panel", { delay: 200, origin: "bottom" });
+sr.reveal(".timeline-image", { delay: 200, origin: "bottom" });
+sr.reveal(".story span", { delay: 200, origin: "bottom" });
+sr.reveal(".story p", { delay: 200, origin: "bottom" });
+sr.reveal(".story h2", { delay: 200, origin: "bottom" });
+
+// Start Info
+sr.reveal(".info h2", { delay: 200, origin: "bottom" });
+sr.reveal(".info p", { delay: 200, origin: "bottom" });
+sr.reveal(".info a", { delay: 200, origin: "bottom" });
+sr.reveal(".card", { delay: 200, origin: "bottom" });
+
+// Start Home
+sr.reveal(".home h2", { delay: 200, origin: "bottom" });
+sr.reveal(".home h6", { delay: 200, origin: "bottom" });
+sr.reveal(".home h3", { delay: 200, origin: "bottom" });
+sr.reveal(".home p", { delay: 200, origin: "bottom" });
+sr.reveal(".home span", { delay: 200, origin: "bottom" });
+sr.reveal(".home h4", { delay: 200, origin: "bottom" });
+sr.reveal(".home img", { delay: 200, origin: "bottom" });
