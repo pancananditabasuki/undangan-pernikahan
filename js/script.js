30d9c72adc4948f8f20921132391ecd3dd8e63bc
Akbar Tawakal Pancanandita Basuki <149308768+pancananditabasuki@users.noreply.github.com>
2024-08-09 15:48:54 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -352,56 +352,273 @@ function createCaption(caption) {
 //       })
 //       .catch((error) => console.error("Error:", error));
 //   });
+//
 
-const form = document.getElementById("kirimucapanselamat");
-const submittedNama = document.getElementById("submittedNama");
-const submittedAlamat = document.getElementById("submittedAlamat");
-const submittedUcapan = document.getElementById("submittedUcapan");
-const submittedStatus = document.getElementById("submittedStatus");
+//
+//
+//
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+//
+// const form = document.getElementById("form");
+// const submittedNama = document.getElementById("submittedNama");
+// const submittedAlamat = document.getElementById("submittedAlamat");
+// const submittedUcapan = document.getElementById("submittedUcapan");
+// const submittedStatus = document.getElementById("submittedStatus");
 
-  const nama = document.getElementById("nama").value;
-  const alamat = document.getElementById("alamat").value;
-  const ucapan = document.getElementById("ucapan").value;
-  const status = document.getElementById("status").value;
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
 
-  localStorage.setItem("nama", nama);
-  localStorage.setItem("alamat", alamat);
-  localStorage.setItem("ucapan", ucapan);
-  localStorage.setItem("status", status);
+//   const nama = document.getElementById("nama").value;
+//   const alamat = document.getElementById("alamat").value;
+//   const ucapan = document.getElementById("ucapan").value;
+//   const status = document.getElementById("status").value;
 
-  submittedNama.textContent = "" + nama;
-  submittedAlamat.textContent = "" + alamat;
-  submittedUcapan.textContent = "" + ucapan;
-  submittedStatus.textContent = "" + status;
-  // submittedData.textContent = "Username saved: " + ucapan;
-  // submittedData.textContent = "Username saved: " + status;
+//   localStorage.setItem("nama", nama);
+//   localStorage.setItem("alamat", alamat);
+//   localStorage.setItem("ucapan", ucapan);
+//   localStorage.setItem("status", status);
+
+//   submittedNama.textContent = "" + nama;
+//   submittedAlamat.textContent = "" + alamat;
+//   submittedUcapan.textContent = "" + ucapan;
+//   submittedStatus.textContent = "" + status;
+// });
+
+// // Retrieve and display the stored username on page load
+// window.onload = function () {
+//   const storedUsername = localStorage.getItem("nama");
+//   const storedAlamat = localStorage.getItem("alamat");
+//   const storedUcapan = localStorage.getItem("ucapan");
+//   const storedStatus = localStorage.getItem("status");
+
+//   if (storedUsername) {
+//     submittedNama.textContent = "" + storedUsername;
+//   }
+//   if (storedAlamat) {
+//     submittedAlamat.textContent = "" + storedAlamat;
+//   }
+//   if (storedUcapan) {
+//     submittedUcapan.textContent = "" + storedUcapan;
+//   }
+//   if (storedStatus) {
+//     submittedStatus.textContent = "" + storedStatus;
+//   }
+// };
+
+// const submittedNama = document.querySelector("nama");
+// const submittedAlamat = document.querySelector("alamat");
+// const submittedUcapan = document.querySelector("ucapan");
+// const submittedStatus = document.querySelector("status");
+// const buttonSubmit = document.querySelector(".btn btn-primary");
+
+// let storeData = [];
+
+// buttonSubmit.addEventListener("click", () => {
+//   storeData.push({
+//     id: Date.now(),
+//     text: submittedNama.value,
+//     // text: submittedAlamat.value,
+//     // text: submittedUcapan.value,
+//     // text: submittedStatus.value,
+//     finish: false,
+//   });
+//   submittedNama.value = "";
+//   // submittedAlamat.value = "";
+//   // submittedUcapan.value = "";
+//   // submittedStatus.value = "";
+// });
+
+// Wait until the DOM is fully loaded
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const commentsSection = document.querySelector(".pagination").parentNode;
+
+  function timeSince(date) {
+    const seconds = Math.floor((new Date() - date) / 1000);
+
+    let interval = Math.floor(seconds / 31536000);
+    if (interval > 1) return interval + " years ago";
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) return interval + " months ago";
+
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) return interval + " days ago";
+
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) return interval + " hours ago";
+
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) return interval + " minutes ago";
+
+    return Math.floor(seconds) + " seconds ago";
+  }
+
+  // Form submission event
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    // Capture form inputs
+    const nameInput = form.querySelector(".nama").value.trim();
+    const messageAlamat = form.querySelector(".alamat").value.trim();
+    const messageInput = form.querySelector(".ucapan").value.trim();
+    const messageKonfirmasi = form.querySelector(".status").value.trim();
+    const timestamp = new Date();
+    // Validate inputs
+    if (
+      !nameInput ||
+      !messageAlamat ||
+      !messageInput ||
+      !messageKonfirmasi ||
+      !timestamp
+    ) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    // Create a new comment element
+    const newComment = document.createElement("div");
+    // newComment.classList.add("comment");
+
+    newComment.innerHTML = `
+    <div class="row justify-content-center">
+       <div class="comment col-md-10 col-10">
+          <div class="comment-header ">
+              <span class="name">${nameInput}</span>
+             <span class="time" data-time="${timestamp}">${timeSince(
+      new Date(timestamp)
+    )}</span>
+          </div>
+          <div class="comment-body"> 
+            ${messageAlamat}
+          </div>
+          <div class="comment-body"> 
+            ${messageInput}
+          </div>
+          <div class="comment-body"> 
+            ${messageKonfirmasi}
+          </div>
+        </div>
+    </div>
+      `;
+
+    // Add the new comment to the comments section before the pagination
+    commentsSection.insertBefore(
+      newComment,
+      document.querySelector(".pagination")
+    );
+
+    // Clear form fields
+    form.reset();
+  });
+
+  setInterval(function () {
+    const times = document.querySelectorAll(".time");
+    times.forEach(function (time) {
+      const timestamp = new Date(time.getAttribute("data-time"));
+      time.textContent = timeSince(timestamp);
+    });
+  }, 60000);
 });
 
-// Retrieve and display the stored username on page load
-window.onload = function () {
-  const storedUsername = localStorage.getItem("nama");
-  const storedAlamat = localStorage.getItem("alamat");
-  const storedUcapan = localStorage.getItem("ucapan");
-  const storedStatus = localStorage.getItem("status");
+// Star
 
-  if (storedUsername) {
-    submittedNama.textContent = "" + storedUsername;
-  }
-  if (storedAlamat) {
-    submittedAlamat.textContent = "" + storedAlamat;
-  }
-  if (storedUcapan) {
-    submittedUcapan.textContent = "" + storedUcapan;
-  }
-  if (storedStatus) {
-    submittedStatus.textContent = "" + storedStatus;
-  }
-};
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("form");
+//   const commentsSection = document.getElementById("pagination");
 
-nama.val("");
-alamat.val("");
-ucapan.val("");
-status.val("");
+//   function timeSince(date) {
+//     const seconds = Math.floor((new Date() - date) / 1000);
+
+//     let interval = Math.floor(seconds / 31536000);
+//     if (interval > 1) return interval + " years ago";
+
+//     interval = Math.floor(seconds / 2592000);
+//     if (interval > 1) return interval + " months ago";
+
+//     interval = Math.floor(seconds / 86400);
+//     if (interval > 1) return interval + " days ago";
+
+//     interval = Math.floor(seconds / 3600);
+//     if (interval > 1) return interval + " hours ago";
+
+//     interval = Math.floor(seconds / 60);
+//     if (interval > 1) return interval + " minutes ago";
+
+//     return Math.floor(seconds) + " seconds ago";
+//   }
+
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault(); // Prevent the default form submission
+
+//     // Capture form inputs
+//     const nameInput = document.getElementById("nama").value.trim();
+//     const messageAlamat = document.getElementById("alamat").value.trim();
+//     const messageInput = document.getElementById("ucapan").value.trim();
+//     const messageKonfirmasi = document.getElementById("status").value.trim();
+//     const timestamp = new Date();
+
+//     // Validate inputs
+//     if (nameInput && messageAlamat && messageInput && messageKonfirmasi) {
+//       addComment(
+//         nameInput,
+//         messageAlamat,
+//         messageInput,
+//         messageKonfirmasi,
+//         timestamp
+//       );
+//     }
+
+//     // Add the new comment to the comments section before the pagination
+//   });
+
+//   function addComment(
+//     nameInput,
+//     messageAlamat,
+//     messageInput,
+//     messageKonfirmasi,
+//     timestamp
+//   ) {
+//     const newComment = document.createElement("div");
+//     // newComment.classList.add("comment");
+
+//     newComment.innerHTML = `
+//     <div class="row justify-content-center">
+//       <div class="comment col-md-10 col-10">
+//           <div class="comment-header">
+//               <span class="name">${nameInput}</span>
+//               <span class="time" data-time="${timestamp}">${timeSince(
+//       new Date(timestamp)
+//     )}</span>
+//             </div>
+//               <div class="comment-body">
+//                 ${messageAlamat}
+//               </div>
+//               <div class="comment-body">
+//                  ${messageInput}
+//              </div>
+//               <div class="comment-body">
+//                  ${messageKonfirmasi}
+//           </div>
+//         </div>
+//        </div>
+//       `;
+//     commentsSection.insertBefore(
+//       newComment,
+//       commentsSection.querySelector(".pagination")
+//     );
+//     // commentsSection.insertBefore(newComment, commentsSection.firstChild);
+//     form.reset();
+//   }
+
+//   // Form submission event
+
+//   setInterval(function () {
+//     const times = document.querySelectorAll(".time");
+//     times.forEach(function (time) {
+//       const timestamp = new Date(time.getAttribute("data-time"));
+//       time.textContent = timeSince(timestamp);
+//     });
+//   }, 60000);
+// });
